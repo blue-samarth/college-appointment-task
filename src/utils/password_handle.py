d37562a92e90cc59451127b8669aef6cba1f03c4
blue-samarth <samarth38work@gmail.com>
2025-01-02 14:41:22 +0530

    password handling

---
+++ src/utils/password_handle.py
@@ -0,0 +1,19 @@
+import logging
+import secrets
+
+from passlib.context import CryptContext
+
+
+class PasswordHandle:
+    def __init__(self):
+        self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def get_password_hash(self, password: str) -> str:
+        return self.pwd_context.hash(password)
+
+    def verify_password(self, plain_password: str, hashed_password: str) -> bool:
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def generate_random_password(self) -> str:
+        return secrets.token_urlsafe(8)
+    
